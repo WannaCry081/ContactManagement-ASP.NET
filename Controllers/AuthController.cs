4196ead98bc425324b45c69025b105bb036718c5
Lirae Que Data <liraedata59@gmail.com>
2023-08-12 15:25:07 +0800

    Add new auth controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{   
+    [ApiController]
+    [Route("/api/auth")]
+    public class AuthController : ControllerBase
+    {
+        
+    }
+}
