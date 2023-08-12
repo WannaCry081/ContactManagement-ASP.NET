5835b9717ddba3d67be45d186290838652feb68e
Lirae Que Data <liraedata59@gmail.com>
2023-08-12 15:25:33 +0800

    Add signin method in auth controller

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -6,6 +6,15 @@ namespace backend.Controllers
     [Route("/api/auth")]
     public class AuthController : ControllerBase
     {
-        
+        public AuthController() {
+
+        }
+
+        [HttpPost("/signin")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public async Task<IActionResult> SignUp() {
+            return Ok("Sign up");
+        }
     }
 }
