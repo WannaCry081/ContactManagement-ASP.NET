455d1debfb298765471c140ec4e72a081947f666
Lirae Que Data <liraedata59@gmail.com>
2023-08-13 12:50:35 +0800

    Add new AuthMapper

---
+++ Mappers/AuthMapper.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using backend.Entities;
+using backend.Models.AuthModels;
+
+namespace backend.Mappers
+{
+    public class AuthMapper : Profile
+    {
+        public AuthMapper() {
+            CreateMap<User, SignUpModel>();
+            CreateMap<User, SignInModel>();
+        }
+    }
+}
