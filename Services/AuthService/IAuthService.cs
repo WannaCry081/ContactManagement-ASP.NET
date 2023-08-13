8bad315c28fb8d93befe6a4107837cd2e1522d58
Lirae Que Data <liraedata59@gmail.com>
2023-08-13 12:42:04 +0800

    Add class methods to IAuthService

--- Services/AuthService/IAuthService.cs
+++ Services/AuthService/IAuthService.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using backend.Models.AuthModels;
 
 namespace backend.Services.AuthService
 {
     public interface IAuthService
     {
-        
+        Task<string> SignUp(SignUpModel request);
+        Task<string> SignIn(SignInModel request);
     }
 }
