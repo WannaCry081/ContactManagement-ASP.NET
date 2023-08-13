7ae19f6c474ee50cd98619f11328998bd44533b9
Lirae Que Data <liraedata59@gmail.com>
2023-08-13 12:30:02 +0800

    Add new SignInModel

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,26 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers
-{   
+{
     [ApiController]
     [Route("/api/auth")]
     public class AuthController : ControllerBase
     {
-        public AuthController() {
+        public AuthController()
+        {
 
         }
 
         [HttpPost("/signin")]
         [Consumes("application/json")]
         [Produces("application/json")]
-        public async Task<IActionResult> SignUp() {
+        public async Task<IActionResult> SignUp()
+        {
             return Ok("Sign up");
         }
 
         [HttpPost("/signin")]
         [Consumes("application/json")]
         [Produces("application/json")]
-        public async Task<IActionResult> SignIn() {
+        public async Task<IActionResult> SignIn()
+        {
             return Ok("Sign in");
         }
     }
