29d0aa9a579841297985e9b446c440987bae9d86
Lirae Que Data <liraedata59@gmail.com>
2023-08-13 12:16:15 +0800

    Add new SignInModel

---
+++ Models/AuthModels/SignInModel.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models.AuthModels
+{
+    public class SignInModel
+    {
+        [EmailAddress(ErrorMessage = "Please enter a valid Email.")]
+        [Required(ErrorMessage = "Email field is Required.")]
+        [MaxLength(100, ErrorMessage = "Maximum length for the Email field is 100 Characters.")]
+        [MinLength(3, ErrorMessage = "Minimum length for the Email field is 3 Characters.")]
+        public string Email { get; set; } = string.Empty;
+        
+        [PasswordPropertyText]
+        [Required(ErrorMessage = "Password field is Required.")]
+        [MaxLength(150, ErrorMessage = "Maximum length for the Password field is 150 Characters.")]
+        [MinLength(6, ErrorMessage = "Minimum length for the Password field is 6 Characters.")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
