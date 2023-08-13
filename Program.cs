f2a538539d00ff9a3ea1b0d6b4023b84308a8f56
Lirae Que Data <liraedata59@gmail.com>
2023-08-13 12:29:13 +0800

    Configure Database setting

--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using backend.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    );
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
 
 var app = builder.Build();
 
