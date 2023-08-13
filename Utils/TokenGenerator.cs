970831e3f9a3437b2b40381b560f79c895519f52
Lirae Que Data <liraedata59@gmail.com>
2023-08-13 13:03:36 +0800

    Add new TokenGenerator class

---
+++ Utils/TokenGenerator.cs
@@ -0,0 +1,39 @@
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using backend.Entities;
+
+namespace backend.Utils
+{
+    public class TokenGenerator
+    {
+        public static string AccessToken(User user, IConfiguration configuration)
+        {
+            List<Claim> claims = new()
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(
+                    configuration.GetSection("AppSettings:JWT_SECRET_KEY").Value!
+                )
+            );
+
+            var credential = new SigningCredentials(
+                key, SecurityAlgorithms.HmacSha512Signature
+            );
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: credential
+            );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
+        }
+    }
+}
