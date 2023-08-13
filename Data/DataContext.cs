c2292a8c73b8bce2452bd3e14d174acba5bffb4b
Lirae Que Data <liraedata59@gmail.com>
2023-08-13 12:28:43 +0800

    Add new DataContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using backend.Entities;
+
+namespace backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
