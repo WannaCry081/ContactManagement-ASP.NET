3fcf59c38313d79281de8dc4d89e4b0f12196076
Lirae Que Data <liraedata59@gmail.com>
2023-08-13 12:11:39 +0800

    Add User Entity

---
+++ Entities/User.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Entities
+{
+    [Table("Users")]
+    public class User
+    {   
+        [Key]
+        public int Id { get; set; }
+        
+        [StringLength(40)]
+        public string FirstName { get; set; } = string.Empty;
+        
+        [StringLength(40)]
+        public string LastName { get; set; } = string.Empty;
+        
+        [StringLength(40)]
+        public string UserName { get; set; } = string.Empty;
+        
+        [StringLength(150)]
+        public string Email { get; set; } = string.Empty;
+        
+        [StringLength(150)]
+        public string PasswordHash { get; set; } = string.Empty;
+        
+        [StringLength(150)]
+        public string PasswordSalt { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
