ec76df94729b21f77964c14e5aa78e7483575967
Lirae Que Data <liraedata59@gmail.com>
2023-08-13 13:44:01 +0800

    Integrate the AuthController -  SignIn()

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using backend.Exceptions;
 using backend.Models.AuthModels;
 using backend.Services.AuthService;
+using System.Linq.Expressions;
 
 namespace backend.Controllers
 {
@@ -45,7 +46,26 @@ namespace backend.Controllers
         [Produces("application/json")]
         public async Task<IActionResult> SignIn([FromBody] SignInModel request)
         {
-            return Ok("Sign in");
+            try
+            {
+                var response = await _authService.SignIn(request);
+                return Ok(response);
+            }
+            catch (NotFoundException ex)
+            {
+                _logger.LogWarning(ex, "An error occurred while attempting to sign in a user.");
+                return NotFound(ex.Message);
+            }
+            catch (UserSignInFailedException ex)
+            {
+                _logger.LogError(ex, "An error occurred while attempting to match Password.");
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical(ex, "An error occurred while attempting to sign in the user.");
+                return Problem(ex.Message);
+            }
         }
     }
 }
