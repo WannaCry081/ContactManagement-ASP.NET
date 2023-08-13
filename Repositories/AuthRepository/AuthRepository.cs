16240e0c90ccbbc827fd9770a17b1d901fffffad
Lirae Que Data <liraedata59@gmail.com>
2023-08-13 13:13:54 +0800

    Implemented IsUserExists()

--- Repositories/AuthRepository/AuthRepository.cs
+++ Repositories/AuthRepository/AuthRepository.cs
@@ -1,12 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using backend.Data;
+using backend.Entities;
 
 namespace backend.Repositories.AuthRepository
 {
-    public class AuthRepository
+    public class AuthRepository : IAuthRepository
     {
-        
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<bool> IsUserExists(User user)
+        {
+            return await _context.Users.Where(
+                (c) => c.UserName.Equals(user.UserName) && 
+                    c.Email.Equals(user.Email)
+            ).AnyAsync();
+        }
+
+        public async Task<bool> AddNewUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
