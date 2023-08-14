09382f2a7c80df3fa266f46bf8a3cfa79a2c43e1
Lirae Que Data <liraedata59@gmail.com>
2023-08-14 10:02:04 +0800

    Add new IUserRepository.cs file

--- Repositories/UserRepository/IUserRepository.cs
+++ Repositories/UserRepository/IUserRepository.cs
@@ -1,7 +1,9 @@
+using backend.Entities;
+
 namespace backend.Repositories.UserRepository
 {
     public interface IUserRepository
     {
-        
+        Task<User?> GetUserByToken(int userId, string email);
     }
 }
