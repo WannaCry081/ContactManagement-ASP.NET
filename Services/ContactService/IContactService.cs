ad7e1ea6e02363ac1f90257dc740203c9eb7b666
Lirae Que Data <liraedata59@gmail.com>
2023-08-14 10:26:26 +0800

    Implemented GetUserContacts() method

--- Services/ContactService/IContactService.cs
+++ Services/ContactService/IContactService.cs
@@ -1,7 +1,14 @@
+using backend.Entities;
+using backend.Models.ContactModels;
+
 namespace backend.Services.ContactService
 {
     public interface IContactService
     {
-        
+        Task<ICollection<GetUserContactModel>> GetUserContacts(User user);
+        Task<GetUserContactModel> GetUserContact(User user, int contactId);
+        Task<GetUserContactModel> CreateUserContact(User user, UpsertUserContactModel request);
+        Task<GetUserContactModel> UpdateUserContact(User user, UpsertUserContactModel request);
+        Task<bool> DeleteUserContact(User user, int contactId);
     }
 }
