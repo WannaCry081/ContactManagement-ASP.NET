1ef336643635be897ce5ad2e3f81114a0b98ce72
Lirae Que Data <liraedata59@gmail.com>
2023-08-14 18:57:32 +0800

    Add XML Document in AuthController

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -5,6 +5,9 @@ using backend.Services.AuthService;
 
 namespace backend.Controllers
 {
+    /// <summary>
+    /// Controller for handling user authentication.
+    /// </summary>
     [ApiController]
     [Route("/api/auth")]
     public class AuthController : ControllerBase
@@ -12,15 +15,48 @@ namespace backend.Controllers
         private readonly ILogger<AuthController> _logger;
         private readonly IAuthService _authService;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AuthController"/> class.
+        /// </summary>
+        /// <param name="logger">The logger instance.</param>
+        /// <param name="authService">The authentication service.</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public AuthController(ILogger<AuthController> logger, IAuthService authService)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _authService = authService ?? throw new ArgumentNullException(nameof(logger));
         }
 
+        /// <summary>
+        /// Sign up a new user. 
+        /// </summary>
+        /// <param name="request">The sign-up request model</param>
+        /// <returns>return a jwt token.</returns>
+        /// <remarks>
+        /// Sample Request:
+        /// 
+        ///     POST /api/auth/signup
+        ///     {
+        ///         "firstName": "John",
+        ///         "lastName" : "Doe",
+        ///         "username" : "JohnDoe123",
+        ///         "email" : "johndoe@example.com",
+        ///         "password" : "password123",
+        ///         "confirmPassword" : "password123"
+        ///     }
+        /// 
+        /// </remarks>
+        /// <response code="200">Successfully created an account.</response>
+        /// <response code="400">Invalid contact details.</response>
+        /// <response code="409">User already exists.</response>
+        /// <response code="500">Internal server error.</response>
         [HttpPost("signup")]
         [Consumes("application/json")]
         [Produces("application/json")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> SignUp([FromBody] SignUpModel request)
         {
             try
@@ -31,7 +67,7 @@ namespace backend.Controllers
             catch (UserSignUpFailedException ex)
             {
                 _logger.LogError(ex, "An error occurred while attempting to add user to database.");
-                return BadRequest(ex.Message);
+                return Conflict(ex.Message);
             }
             catch (Exception ex)
             {
@@ -40,9 +76,33 @@ namespace backend.Controllers
             }
         }
 
+        /// <summary>
+        /// Sign in a user. 
+        /// </summary>
+        /// <param name="request">The sign-in request model.</param>
+        /// <returns>returns a jwt token.</returns>
+        /// <remarks>
+        ///     
+        ///     POST /api/auth/signin
+        ///     {
+        ///         "email" : "johndoe@example.com",
+        ///         "password" : "password123"
+        ///     }
+        ///     
+        /// </remarks>
+        /// <response code="200">Successfully log in user.</response>
+        /// <response code="400">Invalid contact details.</response>
+        /// <response code="401">Invalid user's password.</response>
+        /// <response code="404">User does not exists.</response>
+        /// <response code="500">Internal server error.</response>
         [HttpPost("signin")]
         [Consumes("application/json")]
         [Produces("application/json")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> SignIn([FromBody] SignInModel request)
         {
             try
