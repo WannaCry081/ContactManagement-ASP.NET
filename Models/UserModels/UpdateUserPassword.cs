26b5a25dbfbfd3eddf68dfe3a07333fc41c5cb5b
Lirae Que Data <liraedata59@gmail.com>
2023-08-14 09:43:18 +0800

    Addd new UpdateUserPassword.cs file

---
+++ Models/UserModels/UpdateUserPassword.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models.UserModels
+{
+    public class UpdateUserPassword
+    {
+        [PasswordPropertyText]
+        [Required(ErrorMessage = "Password field is Required.")]
+        [MinLength(6, ErrorMessage = "Minimum length for the Confirm Password field is 6 Characters.")]
+        [MaxLength(150, ErrorMessage = "Maximum length for the Confirm Password field is 150 Characters.")]
+        public string OldPassword { get; set; } = string.Empty;
+
+        [PasswordPropertyText]
+        [Compare("ConfirmNewPassword", ErrorMessage = "Password does not match with Password.")]
+        [Required(ErrorMessage = "Password field is Required.")]
+        [MinLength(6, ErrorMessage = "Minimum length for the Password field is 6 Characters.")]
+        [MaxLength(150, ErrorMessage = "Maximum length for the Password field is 150 Characters.")]
+        public string NewPassword { get; set; } = string.Empty;
+
+        [PasswordPropertyText]
+        [Compare("NewPassword", ErrorMessage = "Confirm New Password does not match with Password.")]
+        [Required(ErrorMessage = "Confirm New Password field is Required.")]
+        [MinLength(6, ErrorMessage = "Minimum length for the Confirm New Password field is 6 Characters.")]
+        [MaxLength(150, ErrorMessage = "Maximum length for the Confirm New Password field is 150 Characters.")]
+        public string ConfirmNewPassword { get; set; } = string.Empty;   
+    }
+}
