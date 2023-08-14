b8178002196fb75c5ff75eb742a0e2f97d6cb70f
Lirae Que Data <liraedata59@gmail.com>
2023-08-14 10:27:54 +0800

    Integrate the GetUserContacts() method

--- Repositories/ContactRepository/ContactRepository.cs
+++ Repositories/ContactRepository/ContactRepository.cs
@@ -1,7 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using backend.Data;
+using backend.Entities;
+
 namespace backend.Repositories.ContactRepository
 {
     public class ContactRepository : IContactRepository
     {
-        
+        private readonly DataContext _context;
+
+        public ContactRepository(DataContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<ICollection<Contact>> GetUserContacts(int userId)
+        {
+            return await _context.Contacts.Where(
+                (c) => c.UserId.Equals(userId)
+            ).ToListAsync();
+        }
     }
 }
