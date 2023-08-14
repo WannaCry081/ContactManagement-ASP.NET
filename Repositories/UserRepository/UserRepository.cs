e9e65aa70b3e30a9e10ad38f05f0d5108ddf0179
Lirae Que Data <liraedata59@gmail.com>
2023-08-14 10:02:32 +0800

    Implement GetUserByToken() method

--- Repositories/UserRepository/UserRepository.cs
+++ Repositories/UserRepository/UserRepository.cs
@@ -1,7 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using backend.Data;
+using backend.Entities;
+
 namespace backend.Repositories.UserRepository
 {
     public class UserRepository : IUserRepository
     {
-        
+        private readonly DataContext _context;
+
+        public UserRepository(DataContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<User?> GetUserByToken(int userId, string email)
+        {
+            return await _context.Users.Where(
+                (c) => c.Id.Equals(userId) && c.Email.Equals(email)
+            ).FirstOrDefaultAsync();
+        }
     }
 }
