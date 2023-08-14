7a341cced2d980bf0313ee3b90c0c24de0f68c58
Lirae Que Data <liraedata59@gmail.com>
2023-08-14 09:42:52 +0800

    Addd new GetUserProfileModel.cs file

---
+++ Models/UserModels/GetUserProfileModel.cs
@@ -0,0 +1,11 @@
+namespace backend.Models.UserModels
+{
+    public class GetUserProfileModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
