9058c52ce7a01e5be295ecd496c5f5949262cef7
Lirae Que Data <liraedata59@gmail.com>
2023-08-14 10:27:12 +0800

    Add new GetUserContacts() prototype

--- Repositories/ContactRepository/IContactRepository.cs
+++ Repositories/ContactRepository/IContactRepository.cs
@@ -1,7 +1,13 @@
+using backend.Entities;
+
 namespace backend.Repositories.ContactRepository
 {
     public interface IContactRepository
     {
-        
+        Task<ICollection<Contact>> GetUserContacts(int userId);
+        // Task<Contact?> GetUserContact();
+        // Task<bool> AddUserContact();
+        // Task<bool> UpdateUserContact();
+        // Task<bool> DeleteUserContact();
     }
 }
