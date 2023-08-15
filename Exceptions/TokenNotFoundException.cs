0750367688272c26a107a2fddc9508d3c064c38b
Lirae Que Data <liraedata59@gmail.com>
2023-08-15 14:55:51 +0800

    Add XML Documentation in TokenNotFoundException

--- Exceptions/TokenNotFoundException.cs
+++ Exceptions/TokenNotFoundException.cs
@@ -1,15 +1,14 @@
 namespace backend.Exceptions
 {
     /// <summary>
-    /// 
+    /// Exception class to represent a token not found error.
     /// </summary>
     public class TokenNotFoundException : Exception
     {
         /// <summary>
-        /// 
+        /// Initializes a new instance of the <see cref="TokenNotFoundException"/> class with a custom error message.
         /// </summary>
-        /// <param name="message"></param>
-        /// <returns></returns>
+        /// <param name="message">The error message.</param>
         public TokenNotFoundException(string message) : base(message) { }
     }
 }
