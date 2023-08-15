d8a6acaf643a955024e72c9bf6890840a201cdd7
Lirae Que Data <liraedata59@gmail.com>
2023-08-15 14:58:41 +0800

    Add XML Documentation in AuthMapper

--- Mappers/AuthMapper.cs
+++ Mappers/AuthMapper.cs
@@ -5,16 +5,19 @@ using backend.Models.AuthModels;
 namespace backend.Mappers
 {
     /// <summary>
-    /// 
+    /// Mapper class to define mappings between User entities and authentication models.
     /// </summary>
     public class AuthMapper : Profile
     {
         /// <summary>
-        /// 
+        /// Initializes a new instance of the <see cref="AuthMapper"/> class.
         /// </summary>
         public AuthMapper() {
+            // Map from User entity to SignUpModel and vice versa
             CreateMap<User, SignUpModel>()
                 .ReverseMap();
+
+            // Map from User entity to SignInModel and vice versa
             CreateMap<User, SignInModel>()
                 .ReverseMap();
         }
