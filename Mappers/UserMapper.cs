1ab91dd2401d9540a4f0bff941eff82d234271c8
Lirae Que Data <liraedata59@gmail.com>
2023-08-15 14:59:21 +0800

    Add XML Documentation in UserMapper

--- Mappers/UserMapper.cs
+++ Mappers/UserMapper.cs
@@ -5,19 +5,24 @@ using backend.Models.UserModels;
 namespace backend.Mappers
 {
     /// <summary>
-    /// 
+    /// Mapper class to define mappings between User entities and user-related models.
     /// </summary>
     public class UserMapper : Profile
     {
         /// <summary>
-        /// 
+        /// Initializes a new instance of the <see cref="UserMapper"/> class.
         /// </summary>
         public UserMapper()
         {
+            // Map from User entity to GetUserProfileModel and vice versa
             CreateMap<User, GetUserProfileModel>()
                 .ReverseMap();
+
+            // Map from User entity to UpdateUserProfileModel and vice versa
             CreateMap<User, UpdateUserProfileModel>()
                 .ReverseMap();
+
+            // Map from User entity to UpdateUserPasswordModel and vice versa
             CreateMap<User, UpdateUserPasswordModel>()
                 .ReverseMap();
         }
