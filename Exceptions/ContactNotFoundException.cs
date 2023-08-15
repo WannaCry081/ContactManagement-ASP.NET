6a38434acbaf1625d4a6fcf78711b88a702c1d47
Lirae Que Data <liraedata59@gmail.com>
2023-08-15 14:55:17 +0800

    Add XML Documentation in ContactNotFoundException

--- Exceptions/ContactNotFoundException.cs
+++ Exceptions/ContactNotFoundException.cs
@@ -1,14 +1,14 @@
 namespace backend.Exceptions
 {
     /// <summary>
-    /// 
+    /// Exception class to represent a contact not found error.
     /// </summary>
     public class ContactNotFoundException : Exception
     {
         /// <summary>
-        /// 
+        /// Initializes a new instance of the <see cref="ContactNotFoundException"/> class with a custom error message.
         /// </summary>
-        /// <param name="message"></param>
+        /// <param name="message">The error message.</param>
         public ContactNotFoundException(string message) : base(message) { }
     }
 }
