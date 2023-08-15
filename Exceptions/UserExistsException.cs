0e7fc5687459f5e12686808453d0495cebe09ed9
Lirae Que Data <liraedata59@gmail.com>
2023-08-15 14:56:25 +0800

    Add XML Documentation in UserExistsException

--- Exceptions/UserExistsException.cs
+++ Exceptions/UserExistsException.cs
@@ -1,15 +1,14 @@
 namespace backend.Exceptions
 {
     /// <summary>
-    /// 
+    /// Exception class to represent a user already exists error.
     /// </summary>
     public class UserExistsException : Exception
     {
         /// <summary>
-        /// 
+        /// Initializes a new instance of the <see cref="UserExistsException"/> class with a custom error message.
         /// </summary>
-        /// <param name="message"></param>
-        /// <returns></returns>
+        /// <param name="message">The error message.</param>
         public UserExistsException(string message) : base(message) { }
     }
 }
