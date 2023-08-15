4717e1f7c234ad01882dc26777e07b5ce563a242
Lirae Que Data <liraedata59@gmail.com>
2023-08-15 16:13:45 +0800

    Add in-line documentation in Program.cs

--- Program.cs
+++ Program.cs
@@ -12,87 +12,110 @@ using backend.Services.AuthService;
 using backend.Services.ContactService;
 using backend.Services.UserService;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")
-    );
-});
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme()
-    {
-        In = ParameterLocation.Header,
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey
-    });
-
-    options.OperationFilter<SecurityRequirementsOperationFilter>();
-
-    options.SwaggerDoc("v1", new OpenApiInfo()
-    {
-        Version = "v1",
-        Title = "Contact Management System API",
-        Description = "A simple Contact Management System API showcasing basic CRUD operations.",
-        Contact = new OpenApiContact()
-        {
-            Name = "Lirae Que A. Data",
-            Url = new Uri("https://github.com/TheDayDreamer01")
-        }
-    });
-
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
-builder.Services.AddAuthentication().AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateIssuerSigningKey = true,
-        ValidateAudience = false,
-        ValidateIssuer = false,
-        IssuerSigningKey = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes(
-                builder.Configuration.GetSection("AppSettings:JWT_SECRET_KEY").Value!
-            )
-        )
-    };
-});
-
-builder.Services.AddScoped<IAuthRepository, AuthRepository>();
-builder.Services.AddScoped<IAuthService, AuthService>();
-
-builder.Services.AddScoped<IContactRepository, ContactRepository>();
-builder.Services.AddScoped<IContactService, ContactService>();
-
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IUserService, UserService>();
-
+// Invoke the ConfigureServices method
+ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // Enable Swagger UI in development environment
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Redirect HTTP requests to HTTPS
 app.UseHttpsRedirection();
 
+// Enable authorization middleware
 app.UseAuthorization();
 
+// Map controllers
 app.MapControllers();
 
+// Run the application
 app.Run();
+
+// Configure services for dependency injection
+void ConfigureServices(IServiceCollection services)
+{
+    builder.Services.AddControllers();
+
+    // Configure database context using SQL Server
+    builder.Services.AddDbContext<DataContext>(options =>
+    {
+        options.UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection")
+        );
+    });
+
+    // Configure AutoMapper
+    builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+    // Add HttpContextAccessor for accessing HttpContext in services
+    builder.Services.AddHttpContextAccessor();
+
+    // Register authentication and authorization services
+    builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+    builder.Services.AddScoped<IAuthService, AuthService>();
+
+    // Register contact repository and service
+    builder.Services.AddScoped<IContactRepository, ContactRepository>();
+    builder.Services.AddScoped<IContactService, ContactService>();
+
+    // Register user repository and service
+    builder.Services.AddScoped<IUserRepository, UserRepository>();
+    builder.Services.AddScoped<IUserService, UserService>();
+
+    // Configure Swagger API documentation
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(options =>
+    {
+        // Configure OAuth2 security definition for Swagger
+        options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme()
+        {
+            In = ParameterLocation.Header,
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey
+        });
+
+        // Apply security filter to operations
+        options.OperationFilter<SecurityRequirementsOperationFilter>();
+
+        // Configure Swagger documentation settings
+        options.SwaggerDoc("v1", new OpenApiInfo()
+        {
+            Version = "v1",
+            Title = "Contact Management System API",
+            Description = "A simple Contact Management System API showcasing basic CRUD operations.",
+            Contact = new OpenApiContact()
+            {
+                Name = "Lirae Que A. Data",
+                Url = new Uri("https://github.com/TheDayDreamer01")
+            }
+        });
+
+        // Include XML comments for documentation
+        var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    });
+
+    // Configure JWT Bearer authentication
+    builder.Services.AddAuthentication().AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuerSigningKey = true,
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(
+                    builder.Configuration.GetSection("AppSettings:JWT_SECRET_KEY").Value!
+                )
+            )
+        };
+    });
+}
