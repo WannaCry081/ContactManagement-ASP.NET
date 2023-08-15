fd6d6735b3ac89764243a2e724a1100ead350ef4
Lirae Que Data <liraedata59@gmail.com>
2023-08-15 15:54:30 +0800

    Add XML Documentation in UserRepository

--- Repositories/UserRepository/IUserRepository.cs
+++ Repositories/UserRepository/IUserRepository.cs
@@ -3,24 +3,24 @@ using backend.Entities;
 namespace backend.Repositories.UserRepository
 {
     /// <summary>
-    /// 
+    /// Repository interface for user operations.
     /// </summary>
     public interface IUserRepository
     {
         /// <summary>
-        /// 
+        /// Gets a user by their token (userId and email).
         /// </summary>
-        /// <param name="userId"></param>
-        /// <param name="email"></param>
-        /// <returns></returns>
+        /// <param name="userId">The user's ID.</param>
+        /// <param name="email">The user's email.</param>
+        /// <returns>The user if found, or null.</returns>
         Task<User?> GetUserByToken(int userId, string email);
 
         /// <summary>
-        /// 
+        /// Updates a user's profile details.
         /// </summary>
-        /// <param name="user"></param>
-        /// <param name="newUserDetails"></param>
-        /// <returns></returns>
+        /// <param name="user">The user to be updated.</param>
+        /// <param name="newUserDetails">The new user details.</param>
+        /// <returns>True if the update was successful, false otherwise.</returns>
         Task<bool> UpdateUserProfile(User user, User newUserDetails);
     }
 }
--- Repositories/UserRepository/UserRepository.cs
+++ Repositories/UserRepository/UserRepository.cs
@@ -5,28 +5,23 @@ using backend.Entities;
 namespace backend.Repositories.UserRepository
 {
     /// <summary>
-    /// 
+    /// Repository class for user operations.
     /// </summary>
     public class UserRepository : IUserRepository
     {
         private readonly DataContext _context;
 
         /// <summary>
-        /// 
+        /// Initializes a new instance of the `UserRepository` class.
         /// </summary>
-        /// <param name="context"></param>
-        /// <exception cref="ArgumentNullException"></exception>
+        /// <param name="context">The database context.</param>
+        /// <exception cref="ArgumentNullException">Thrown if the context is null.</exception>
         public UserRepository(DataContext context)
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="userId"></param>
-        /// <param name="email"></param>
-        /// <returns></returns>
+        /// <inheritdoc/>
         public async Task<User?> GetUserByToken(int userId, string email)
         {
             return await _context.Users.Where(
@@ -34,12 +29,7 @@ namespace backend.Repositories.UserRepository
             ).FirstOrDefaultAsync();
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="user"></param>
-        /// <param name="newUserDetails"></param>
-        /// <returns></returns>
+        /// <inheritdoc/>
         public async Task<bool> UpdateUserProfile(User user, User newUserDetails) {
             user.FirstName = newUserDetails.FirstName;
             user.LastName = newUserDetails.LastName;
