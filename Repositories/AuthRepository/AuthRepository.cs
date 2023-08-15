3c2d8f2d485f9313c02bc876e867d5b575314baf
Lirae Que Data <liraedata59@gmail.com>
2023-08-15 15:27:32 +0800

    Add XML Documentation in AuthRepository

--- Repositories/AuthRepository/AuthRepository.cs
+++ Repositories/AuthRepository/AuthRepository.cs
@@ -1,32 +1,27 @@
 using Microsoft.EntityFrameworkCore;
 using backend.Data;
 using backend.Entities;
-using Microsoft.Identity.Client;
 
 namespace backend.Repositories.AuthRepository
 {
     /// <summary>
-    /// 
+    /// Repository class for user authentication operations.
     /// </summary>
     public class AuthRepository : IAuthRepository
     {
         private readonly DataContext _context;
 
         /// <summary>
-        /// 
+        /// Initializes a new instance of the `AuthRepository` class.
         /// </summary>
-        /// <param name="context"></param>
-        /// <exception cref="ArgumentNullException"></exception>
+        /// <param name="context">The database context.</param>
+        /// <exception cref="ArgumentNullException">Thrown if the context is null.</exception>
         public AuthRepository(DataContext context)
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="user"></param>
-        /// <returns></returns>
+        /// <inheritdoc/>
         public async Task<bool> IsUserExists(User user)
         {
             return await _context.Users.Where(
@@ -35,11 +30,7 @@ namespace backend.Repositories.AuthRepository
             ).AnyAsync();
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="user"></param>
-        /// <returns></returns>
+        /// <inheritdoc/>
         public async Task<bool> AddNewUser(User user)
         {
             _context.Users.Add(user);
@@ -47,11 +38,7 @@ namespace backend.Repositories.AuthRepository
             return result > 0;
         }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="email"></param>
-        /// <returns></returns>
+        /// <inheritdoc/>
         public async Task<User?> GetUserByEmail(string email)
         {
             return await _context.Users.FirstOrDefaultAsync(
--- Repositories/AuthRepository/IAuthRepository.cs
+++ Repositories/AuthRepository/IAuthRepository.cs
@@ -3,29 +3,29 @@ using backend.Entities;
 namespace backend.Repositories.AuthRepository
 {
     /// <summary>
-    /// 
+    /// Interface for user authentication repository.
     /// </summary>
     public interface IAuthRepository
     {
         /// <summary>
-        /// 
+        /// Check if a user with the given details exists.
         /// </summary>
-        /// <param name="user"></param>
-        /// <returns></returns>
+        /// <param name="user">The user to check.</param>
+        /// <returns>Returns true if the user exists, false otherwise.</returns>
         Task<bool> IsUserExists(User user);
-        
+
         /// <summary>
-        /// 
+        /// Add a new user to the repository.
         /// </summary>
-        /// <param name="user"></param>
-        /// <returns></returns>
+        /// <param name="user">The user to add.</param>
+        /// <returns>Returns true if the user was added successfully.</returns>
         Task<bool> AddNewUser(User user);
 
         /// <summary>
-        /// 
+        /// Get a user by their email.
         /// </summary>
-        /// <param name="email"></param>
-        /// <returns></returns>
+        /// <param name="email">The email of the user.</param>
+        /// <returns>Returns the user with the specified email, or null if not found.</returns>
         Task<User?> GetUserByEmail(string email);
     }
 }
