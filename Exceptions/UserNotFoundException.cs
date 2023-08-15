746079560794c4e3e14aacb504faa4cf6df18e97
Lirae Que Data <liraedata59@gmail.com>
2023-08-15 14:56:45 +0800

    Add XML Documentation in UserNotFoundException

--- Exceptions/UserNotFoundException.cs
+++ Exceptions/UserNotFoundException.cs
@@ -1,14 +1,14 @@
 namespace backend.Exceptions
 {
     /// <summary>
-    /// 
+    /// Exception class to represent a user not found error.
     /// </summary>
     public class UserNotFoundException : Exception
     {
         /// <summary>
-        /// 
+        /// Initializes a new instance of the <see cref="UserNotFoundException"/> class with a custom error message.
         /// </summary>
-        /// <param name="message"></param>
+        /// <param name="message">The error message.</param>
         public UserNotFoundException(string message) : base(message) { }
     }
 }
