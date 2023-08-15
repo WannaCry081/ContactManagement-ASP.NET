6a6dbd1814bc17cf154b43e963bf159e5024df7d
Lirae Que Data <liraedata59@gmail.com>
2023-08-15 15:08:33 +0800

    Add XML Documentation in GetUserContactModel

--- Models/ContactModels/GetUserContactModel.cs
+++ Models/ContactModels/GetUserContactModel.cs
@@ -1,37 +1,37 @@
 namespace backend.Models.ContactModels
 {
     /// <summary>
-    /// 
+    /// Model for retrieving user contact information.
     /// </summary>
     public class GetUserContactModel
     {
         /// <summary>
-        /// 
+        /// Gets or sets the ID of the contact.
         /// </summary>
         public int Id { get; set; }
-        
+
         /// <summary>
-        /// 
+        /// Gets or sets the first name of the contact.
         /// </summary>
         public string FirstName { get; set; } = string.Empty;
 
         /// <summary>
-        /// 
+        /// Gets or sets the last name of the contact.
         /// </summary>
         public string LastName { get; set; } = string.Empty;
 
         /// <summary>
-        /// 
+        /// Gets or sets the email of the contact.
         /// </summary>
         public string Email { get; set; } = string.Empty;
 
         /// <summary>
-        /// 
+        /// Gets or sets the phone number of the contact.
         /// </summary>
         public string PhoneNo { get; set; } = string.Empty;
 
         /// <summary>
-        /// 
+        /// Gets or sets the address of the contact.
         /// </summary>
         public string Address { get; set; } = string.Empty;
     }
