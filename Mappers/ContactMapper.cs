f04238b5b45e2b16d8147d3e2eb3988502888e4c
Lirae Que Data <liraedata59@gmail.com>
2023-08-15 14:58:19 +0800

    Add XML Documentation in ContactMapper

--- Mappers/ContactMapper.cs
+++ Mappers/ContactMapper.cs
@@ -5,17 +5,20 @@ using backend.Models.ContactModels;
 namespace backend.Mappers
 {
     /// <summary>
-    /// 
+    /// Mapper class to define mappings between Contact entities and models.
     /// </summary>
     public class ContactMapper : Profile
     {
         /// <summary>
-        /// 
+        /// Initializes a new instance of the <see cref="ContactMapper"/> class.
         /// </summary>
         public ContactMapper()
         {
+            // Map from Contact entity to GetUserContactModel
             CreateMap<Contact, GetUserContactModel>()
                 .ReverseMap();
+
+            // Map from Contact entity to UpsertUserContactModel and vice versa
             CreateMap<Contact, UpsertUserContactModel>()
                 .ReverseMap();
         }
