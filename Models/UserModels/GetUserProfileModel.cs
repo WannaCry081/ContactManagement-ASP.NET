e4f308e0e03ebdaaa34128fcd7bdeb1e0cfa085b
Lirae Que Data <liraedata59@gmail.com>
2023-08-15 15:09:47 +0800

    Add XML Documentation in GetUserProfileModel

--- Models/UserModels/GetUserProfileModel.cs
+++ Models/UserModels/GetUserProfileModel.cs
@@ -1,32 +1,32 @@
 namespace backend.Models.UserModels
 {
     /// <summary>
-    /// 
+    /// Model for retrieving user profile information.
     /// </summary>
     public class GetUserProfileModel
     {
         /// <summary>
-        /// 
+        /// Gets or sets the ID of the user.
         /// </summary>
         public int Id { get; set; }
 
         /// <summary>
-        /// 
+        /// Gets or sets the first name of the user.
         /// </summary>
         public string FirstName { get; set; } = string.Empty;
 
         /// <summary>
-        /// 
+        /// Gets or sets the last name of the user.
         /// </summary>
         public string LastName { get; set; } = string.Empty;
 
         /// <summary>
-        /// 
+        /// Gets or sets the username of the user.
         /// </summary>
         public string UserName { get; set; } = string.Empty;
 
         /// <summary>
-        /// 
+        /// Gets or sets the email of the user.
         /// </summary>
         public string Email { get; set; } = string.Empty;
     }
