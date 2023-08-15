c6d34c624d650ded5a5ffc536c29dea8edb63764
Lirae Que Data <liraedata59@gmail.com>
2023-08-15 15:01:40 +0800

    Add XML Documentation in SignInModel

--- Models/AuthModels/SignInModel.cs
+++ Models/AuthModels/SignInModel.cs
@@ -4,26 +4,26 @@ using System.ComponentModel.DataAnnotations;
 namespace backend.Models.AuthModels
 {
     /// <summary>
-    /// 
+    /// Data model for user sign-in.
     /// </summary>
     public class SignInModel
     {
         /// <summary>
-        /// 
+        /// Gets or sets the user's email.
         /// </summary>
         [EmailAddress(ErrorMessage = "Please enter a valid Email.")]
-        [Required(ErrorMessage = "Email field is Required.")]
-        [MaxLength(100, ErrorMessage = "Maximum length for the Email field is 100 Characters.")]
-        [MinLength(3, ErrorMessage = "Minimum length for the Email field is 3 Characters.")]
+        [Required(ErrorMessage = "Email field is required.")]
+        [MaxLength(100, ErrorMessage = "Maximum length for the Email field is 100 characters.")]
+        [MinLength(3, ErrorMessage = "Minimum length for the Email field is 3 characters.")]
         public string Email { get; set; } = string.Empty;
 
         /// <summary>
-        /// 
+        /// Gets or sets the user's password.
         /// </summary>
         [PasswordPropertyText]
-        [Required(ErrorMessage = "Password field is Required.")]
-        [MaxLength(150, ErrorMessage = "Maximum length for the Password field is 150 Characters.")]
-        [MinLength(6, ErrorMessage = "Minimum length for the Password field is 6 Characters.")]
+        [Required(ErrorMessage = "Password field is required.")]
+        [MaxLength(150, ErrorMessage = "Maximum length for the Password field is 150 characters.")]
+        [MinLength(6, ErrorMessage = "Minimum length for the Password field is 6 characters.")]
         public string Password { get; set; } = string.Empty;
     }
 }
