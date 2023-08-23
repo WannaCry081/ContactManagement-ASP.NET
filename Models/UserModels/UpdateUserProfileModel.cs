61f401ef1b838b8c351108e207d6efd5c332affe
Lirae Que Data <liraedata59@gmail.com>
2023-08-23 22:26:12 +0800

    Modify UpdateUserProfileModel Data Annotation

--- Models/UserModels/UpdateUserProfileModel.cs
+++ Models/UserModels/UpdateUserProfileModel.cs
@@ -11,24 +11,23 @@ namespace backend.Models.UserModels
         /// Gets or sets the updated first name of the user.
         /// </summary>
         [Required(ErrorMessage = "First Name field is required.")]
-        [MaxLength(40, ErrorMessage = "Maximum length for the First Name field is 40 characters.")]
-        [MinLength(1, ErrorMessage = "Minimum length for the First Name field is 1 character.")]
+        [RegularExpression(@"^[\w\d\s]+$", ErrorMessage = "Invalid first name.")]
+        [StringLength(40, ErrorMessage = "First Name must be between {2} and {1} characters long.", MinimumLength = 2)]
         public string FirstName { get; set; } = string.Empty;
 
         /// <summary>
         /// Gets or sets the updated last name of the user.
         /// </summary>
         [Required(ErrorMessage = "Last Name field is required.")]
-        [MaxLength(40, ErrorMessage = "Maximum length for the Last Name field is 40 characters.")]
-        [MinLength(1, ErrorMessage = "Minimum length for the Last Name field is 1 character.")]
+        [RegularExpression(@"^[\w\d\s]+$", ErrorMessage = "Invalid last name.")]
+        [StringLength(40, ErrorMessage = "Last Name must be between {2} and {1} characters long.", MinimumLength = 2)]
         public string LastName { get; set; } = string.Empty;
 
         /// <summary>
         /// Gets or sets the updated username of the user.
         /// </summary>
         [Required(ErrorMessage = "Username field is required.")]
-        [MaxLength(40, ErrorMessage = "Maximum length for the Username field is 40 characters.")]
-        [MinLength(1, ErrorMessage = "Minimum length for the Username field is 1 character.")]
+        [StringLength(40, ErrorMessage = "Username must be between {2} and {1} characters long.", MinimumLength = 2)]
         public string UserName { get; set; } = string.Empty;
     }
 }
