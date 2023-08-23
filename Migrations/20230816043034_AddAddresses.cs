5d603072502ddf2c3c4f25651948aaaa709c85ea
Lirae Que Data <liraedata59@gmail.com>
2023-08-24 00:12:11 +0800

    Modify - Update Migrations

---
+++ Migrations/20230816043034_AddAddresses.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAddresses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Contacts",
+                newName: "DeliveryAddress");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BillingAddress",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BillingAddress",
+                table: "Contacts");
+
+            migrationBuilder.RenameColumn(
+                name: "DeliveryAddress",
+                table: "Contacts",
+                newName: "Address");
+        }
+    }
+}
