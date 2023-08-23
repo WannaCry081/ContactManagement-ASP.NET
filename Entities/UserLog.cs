e706f536e00133c028775de434cf18e154d80cb3
Lirae Que Data <liraedata59@gmail.com>
2023-08-23 22:49:54 +0800

    Add XML Documentation in UserLog entity

--- Entities/UserLog.cs
+++ Entities/UserLog.cs
@@ -5,39 +5,39 @@ using backend.Entities.Types;
 namespace backend.Entities
 {
     /// <summary>
-    /// 
+    /// Represents a user log entry.
     /// </summary>
     [Table("UserLogs")]
     public class UserLog
     {
         /// <summary>
-        /// 
+        /// Gets or sets the unique identifier for the user log.
         /// </summary>
         [Key]
         public int Id { get; set; }
 
         /// <summary>
-        /// 
+        /// Gets or sets the ID of the user associated with the log entry.
         /// </summary>
         public int UserId { get; set; }
 
         /// <summary>
-        /// 
+        /// Gets or sets the user associated with the log entry.
         /// </summary>
         public User? User { get; set; }
 
         /// <summary>
-        /// 
+        /// Gets or sets the description of the event associated with the log entry.
         /// </summary>
         public string EventDescription { get; set; } = string.Empty;
 
         /// <summary>
-        /// 
+        /// Gets or sets the type of the event associated with the log entry.
         /// </summary>
         public UserEventType EventType { get; set; }
 
         /// <summary>
-        /// 
+        /// Gets or sets the timestamp of the event associated with the log entry.
         /// </summary>
         public DateTime EventTime { get; set; } = DateTime.Now;
     }
