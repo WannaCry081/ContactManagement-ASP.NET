543bad141914f765bc7a1a918940670895cb3c46
Lirae Que Data <liraedata59@gmail.com>
2023-08-23 22:34:22 +0800

    Add XML Documentation in UserEventType enum

--- Entities/Types/UserEventType.cs
+++ Entities/Types/UserEventType.cs
@@ -3,18 +3,18 @@ using System.Text.Json.Serialization;
 namespace backend.Entities.Types
 {
     /// <summary>
-    /// 
-    /// </summary> 
+    /// Represents the type of user event.
+    /// </summary>
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum UserEventType
     {
         /// <summary>
-        /// 
+        /// Indicates a user sign-up event.
         /// </summary>
         SignUp = 0,
 
         /// <summary>
-        /// 
+        /// Indicates a user sign-in event.
         /// </summary>
         SignIn = 1
     }
