d4cc1d2f21dd56674153380b786a88bf89ff1403
Lirae Que Data <liraedata59@gmail.com>
2023-08-23 22:33:52 +0800

    Add XML Documentation in ContactEventType enum

--- Entities/Types/ContactEventType.cs
+++ Entities/Types/ContactEventType.cs
@@ -3,28 +3,28 @@ using System.Text.Json.Serialization;
 namespace backend.Entities.Types
 {
     /// <summary>
-    /// 
+    /// Represents the type of contact event.
     /// </summary>
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum ContactEventType
     {
         /// <summary>
-        /// 
+        /// Indicates a contact creation event.
         /// </summary>
         Create = 0,
-        
+
         /// <summary>
-        /// 
+        /// Indicates a contact retrieval event.
         /// </summary>
         Retrieve = 1,
-        
+
         /// <summary>
-        /// 
+        /// Indicates a contact update event.
         /// </summary>
         Update = 2,
-        
+
         /// <summary>
-        /// 
+        /// Indicates a contact deletion event.
         /// </summary>
         Delete = 3
     }
