69354c83644a3d4c520c538bf646416b36a070d8
Lirae Que Data <liraedata59@gmail.com>
2023-08-24 23:33:48 +0800

    Add XML Documentation in IContactLogService

--- Services/ContactLogService/IContactLogService.cs
+++ Services/ContactLogService/IContactLogService.cs
@@ -4,18 +4,18 @@ using backend.Entities.Types;
 namespace backend.Services.ContactLogService
 {
     /// <summary>
-    /// 
+    /// Represents a service for managing contact log operations.
     /// </summary>
     public interface IContactLogService
     {
         /// <summary>
-        /// 
+        /// Logs a contact-related event.
         /// </summary>
-        /// <param name="user"></param>
-        /// <param name="contact"></param>
-        /// <param name="description"></param>
-        /// <param name="type"></param>
-        /// <returns></returns>
+        /// <param name="user">The user associated with the event.</param>
+        /// <param name="contact">The contact for whom the event is logged.</param>
+        /// <param name="description">The description of the event.</param>
+        /// <param name="type">The type of the event.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         Task LogContact(User user, Contact contact, string description, ContactEventType type);
     }
 }
