4525b179f446a1643714650e025923bdaffc0418
Lirae Que Data <liraedata59@gmail.com>
2023-08-24 23:10:58 +0800

    Implemented LogUserAuthentication() method

--- Services/UserLogService/IUserLogService.cs
+++ Services/UserLogService/IUserLogService.cs
@@ -1,7 +1,20 @@
+using backend.Entities;
+using backend.Entities.Types;
+
 namespace backend.Services.UserLogService
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface IUserLogService
     {
-        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="description"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        Task LogUserAuthentication(User user, string description, UserEventType type);
     }
 }
