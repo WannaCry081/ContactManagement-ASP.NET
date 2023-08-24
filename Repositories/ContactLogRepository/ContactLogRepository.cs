08cce80919fc064c06394dc7a8c5a3822f5ac526
Lirae Que Data <liraedata59@gmail.com>
2023-08-24 23:34:32 +0800

    Add XML Documentation in ContactLogRepository

--- Repositories/ContactLogRepository/ContactLogRepository.cs
+++ Repositories/ContactLogRepository/ContactLogRepository.cs
@@ -4,18 +4,17 @@ using backend.Entities;
 namespace backend.Repositories.ContactLogRepository
 {
     /// <summary>
-    /// 
-    /// </summary> <summary>
-    /// 
+    /// Repository class for managing contact log data.
     /// </summary>
     public class ContactLogRepository : IContactLogRepository
     {
         private readonly DataContext _context;
 
         /// <summary>
-        /// 
+        /// Initializes a new instance of the <see cref="ContactLogRepository"/> class.
         /// </summary>
-        /// <param name="context"></param>
+        /// <param name="context">The database context.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="context"/> is null.</exception>
         public ContactLogRepository(DataContext context)
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
