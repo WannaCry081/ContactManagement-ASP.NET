373ddcc05772a930d574848d9c6ce7d268f87ab6
Lirae Que Data <liraedata59@gmail.com>
2023-08-24 23:10:41 +0800

    Implemented AddUserLog() method

--- Repositories/UserLogRepository/IUserLogRepository.cs
+++ Repositories/UserLogRepository/IUserLogRepository.cs
@@ -1,7 +1,21 @@
+using backend.Entities;
+
 namespace backend.Repositories.UserLogRepository
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface IUserLogRepository
     {
-        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userLog"></param>
+        /// <returns></returns> <summary>
+        /// 
+        /// </summary>
+        /// <param name="userLog"></param>
+        /// <returns></returns>
+        Task AddUserLog(UserLog userLog);
     }
 }
