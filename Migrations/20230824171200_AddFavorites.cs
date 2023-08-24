beff519a9ae998627e4fd6c1e1e1f5e4922a4e02
Lirae Que Data <liraedata59@gmail.com>
2023-08-25 01:12:51 +0800

    Update Migrations

---
+++ Migrations/20230824171200_AddFavorites.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFavorites : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBlock",
+                table: "Contacts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFavorite",
+                table: "Contacts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsBlock",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "IsFavorite",
+                table: "Contacts");
+        }
+    }
+}
