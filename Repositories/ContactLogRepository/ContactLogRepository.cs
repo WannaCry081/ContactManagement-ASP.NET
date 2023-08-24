cdbf1944648116b7aed0a04b27f185e6460c64df
Lirae Que Data <liraedata59@gmail.com>
2023-08-24 23:26:27 +0800

    Integrate AddContactLog() method

--- Repositories/ContactLogRepository/ContactLogRepository.cs
+++ Repositories/ContactLogRepository/ContactLogRepository.cs
@@ -1,7 +1,31 @@
+using backend.Data;
+using backend.Entities;
+
 namespace backend.Repositories.ContactLogRepository
 {
+    /// <summary>
+    /// 
+    /// </summary> <summary>
+    /// 
+    /// </summary>
     public class ContactLogRepository : IContactLogRepository
     {
-        
+        private readonly DataContext _context;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        public ContactLogRepository(DataContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        /// <inheritdoc/>
+        public async Task AddContactLog(ContactLog contactLog)
+        {
+            _context.ContactLogs.Add(contactLog);
+            await _context.SaveChangesAsync();
+        }
     }
 }
--- Repositories/ContactLogRepository/IContactLogRepository.cs
+++ Repositories/ContactLogRepository/IContactLogRepository.cs
@@ -1,7 +1,19 @@
+using backend.Entities;
+
 namespace backend.Repositories.ContactLogRepository
 {
+    /// <summary>
+    /// 
+    /// </summary> <summary>
+    /// 
+    /// </summary>
     public interface IContactLogRepository
     {
-        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="contactLog"></param>
+        /// <returns></returns>
+        Task AddContactLog(ContactLog contactLog);
     }
 }
