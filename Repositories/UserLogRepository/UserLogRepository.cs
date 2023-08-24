0fe3eea3071750248e0a6f0e01343a1e8276e700
Lirae Que Data <liraedata59@gmail.com>
2023-08-24 23:11:32 +0800

    Integrate AddUserLog() method

--- Repositories/UserLogRepository/UserLogRepository.cs
+++ Repositories/UserLogRepository/UserLogRepository.cs
@@ -1,7 +1,29 @@
+using backend.Data;
+using backend.Entities;
+
 namespace backend.Repositories.UserLogRepository
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class UserLogRepository : IUserLogRepository
     {
-        
+        private readonly DataContext _context;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        public UserLogRepository(DataContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        /// <inheritdoc /> 
+        public async Task AddUserLog(UserLog userLog)
+        {
+            _context.UserLogs.Add(userLog);
+            await _context.SaveChangesAsync();
+        }
     }
 }
