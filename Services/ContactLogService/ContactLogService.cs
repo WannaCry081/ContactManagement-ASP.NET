7c904f24c585dee01acec0a462084926b221ddb5
Lirae Que Data <liraedata59@gmail.com>
2023-08-24 23:34:01 +0800

    Add XML Documentation in ContactLogService

--- Services/ContactLogService/ContactLogService.cs
+++ Services/ContactLogService/ContactLogService.cs
@@ -5,22 +5,23 @@ using backend.Repositories.ContactLogRepository;
 namespace backend.Services.ContactLogService
 {
     /// <summary>
-    /// 
+    /// Service class for managing contact log operations.
     /// </summary>
     public class ContactLogService : IContactLogService
     {
         private readonly IContactLogRepository _contactLogRepository;
 
         /// <summary>
-        /// 
+        /// Initializes a new instance of the <see cref="ContactLogService"/> class.
         /// </summary>
-        /// <param name="contactLogRepository"></param>
+        /// <param name="contactLogRepository">The contact log repository.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="contactLogRepository"/> is null.</exception>
         public ContactLogService(IContactLogRepository contactLogRepository)
         {
             _contactLogRepository = contactLogRepository ?? throw new ArgumentNullException(nameof(contactLogRepository));
         }
 
-        /// <inheritdoc/> 
+        /// <inheritdoc />
         public async Task LogContact(User user, Contact contact, string description, ContactEventType type)
         {
             var contactLog = new ContactLog()
