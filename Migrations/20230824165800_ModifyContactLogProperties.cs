a998a16fdd4d9ae8f148b2a3d3022036f84eeb9d
Lirae Que Data <liraedata59@gmail.com>
2023-08-25 00:59:10 +0800

    Modify - Add new 'ModifyContactLogProperties' Migration

---
+++ Migrations/20230824165800_ModifyContactLogProperties.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyContactLogProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ContactLogs_Contacts_ContactId",
+                table: "ContactLogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ContactLogs_ContactId",
+                table: "ContactLogs");
+
+            migrationBuilder.DropColumn(
+                name: "ContactId",
+                table: "ContactLogs");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ContactId",
+                table: "ContactLogs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContactLogs_ContactId",
+                table: "ContactLogs",
+                column: "ContactId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ContactLogs_Contacts_ContactId",
+                table: "ContactLogs",
+                column: "ContactId",
+                principalTable: "Contacts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
