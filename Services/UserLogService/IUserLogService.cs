428a76fd6f6209c923679c728063242c8c66d84e
Lirae Que Data <liraedata59@gmail.com>
2023-08-24 23:33:33 +0800

    Add XML Documentation in IUserLogService

--- Services/UserLogService/IUserLogService.cs
+++ Services/UserLogService/IUserLogService.cs
@@ -4,17 +4,17 @@ using backend.Entities.Types;
 namespace backend.Services.UserLogService
 {
     /// <summary>
-    /// 
+    /// Represents a service for managing user log operations.
     /// </summary>
     public interface IUserLogService
     {
         /// <summary>
-        /// 
+        /// Logs a user authentication event.
         /// </summary>
-        /// <param name="user"></param>
-        /// <param name="description"></param>
-        /// <param name="type"></param>
-        /// <returns></returns>
+        /// <param name="user">The user for whom the event is logged.</param>
+        /// <param name="description">The description of the event.</param>
+        /// <param name="type">The type of the event.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         Task LogUserAuthentication(User user, string description, UserEventType type);
     }
 }
