4b963e91c4eafe7436d2ba81ad02ed4bd508961c
Lirae Que Data <liraedata59@gmail.com>
2023-08-25 01:01:30 +0800

    Modify - Remove Contacts Properties

--- Entities/ContactLog.cs
+++ Entities/ContactLog.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using backend.Entities.Types;
 
 namespace backend.Entities
 {
@@ -27,16 +26,6 @@ namespace backend.Entities
         public User? User { get; set; }
 
         /// <summary>
-        /// Gets or sets the ID of the associated contact.
-        /// </summary> 
-        public int ContactId { get; set; }
-
-        /// <summary>
-        /// Gets or sets the associated contact.
-        /// </summary>
-        public Contact? Contact { get; set; }
-
-        /// <summary>
         /// Gets or sets the description of the event associated with the contact log entry.
         /// </summary>
         public string EventDescription { get; set; } = string.Empty;
@@ -44,7 +33,7 @@ namespace backend.Entities
         /// <summary>
         /// Gets or sets the type of the event associated with the contact log entry.
         /// </summary>
-        public ContactEventType? EventType { get; set; }
+        public string EventType { get; set; } = string.Empty;
 
         /// <summary>
         /// Gets or sets the timestamp of the event associated with the contact log entry.
