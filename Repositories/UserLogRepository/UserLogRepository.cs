d2da03c62e7db611a2a102637f0625220cf5696b
Lirae Que Data <liraedata59@gmail.com>
2023-08-24 23:30:15 +0800

    Add XML Documentation in UserLogRepository

--- Repositories/UserLogRepository/UserLogRepository.cs
+++ Repositories/UserLogRepository/UserLogRepository.cs
@@ -4,22 +4,23 @@ using backend.Entities;
 namespace backend.Repositories.UserLogRepository
 {
     /// <summary>
-    /// 
+    /// Repository class for managing user log data.
     /// </summary>
     public class UserLogRepository : IUserLogRepository
     {
         private readonly DataContext _context;
 
         /// <summary>
-        /// 
+        /// Initializes a new instance of the <see cref="UserLogRepository"/> class.
         /// </summary>
-        /// <param name="context"></param>
+        /// <param name="context">The database context.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="context"/> is null.</exception>
         public UserLogRepository(DataContext context)
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
-        /// <inheritdoc /> 
+        /// <inheritdoc/>
         public async Task AddUserLog(UserLog userLog)
         {
             _context.UserLogs.Add(userLog);
