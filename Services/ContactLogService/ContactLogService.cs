e88a3e89bc4367e60a9ba1559ed4b2510103b1a5
Lirae Que Data <liraedata59@gmail.com>
2023-08-24 23:26:43 +0800

    Integrate ContactLogService() method

--- Services/ContactLogService/ContactLogService.cs
+++ Services/ContactLogService/ContactLogService.cs
@@ -1,7 +1,39 @@
+using backend.Entities;
+using backend.Entities.Types;
+using backend.Repositories.ContactLogRepository;
+
 namespace backend.Services.ContactLogService
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class ContactLogService : IContactLogService
     {
-        
+        private readonly IContactLogRepository _contactLogRepository;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="contactLogRepository"></param>
+        public ContactLogService(IContactLogRepository contactLogRepository)
+        {
+            _contactLogRepository = contactLogRepository ?? throw new ArgumentNullException(nameof(contactLogRepository));
+        }
+
+        /// <inheritdoc/> 
+        public async Task LogContact(User user, Contact contact, string description, ContactEventType type)
+        {
+            var contactLog = new ContactLog()
+            {
+                UserId = user.Id,
+                User = user,
+                ContactId = contact.Id,
+                Contact = contact,
+                EventDescription = description,
+                EventType = type
+            };
+
+            await _contactLogRepository.AddContactLog(contactLog);
+        }
     }
 }
--- Services/ContactLogService/IContactLogService.cs
+++ Services/ContactLogService/IContactLogService.cs
@@ -1,7 +1,21 @@
+using backend.Entities;
+using backend.Entities.Types;
+
 namespace backend.Services.ContactLogService
 {
-    public interface IContactLogService 
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IContactLogService
     {
-        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="contact"></param>
+        /// <param name="description"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        Task LogContact(User user, Contact contact, string description, ContactEventType type);
     }
 }
