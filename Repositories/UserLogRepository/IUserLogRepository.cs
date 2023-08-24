c7d4421408de041e3f566bb84bf19ea352ac2b15
Lirae Que Data <liraedata59@gmail.com>
2023-08-24 23:29:58 +0800

    Add XML Documentation in IUserLogRepository

--- Repositories/UserLogRepository/IUserLogRepository.cs
+++ Repositories/UserLogRepository/IUserLogRepository.cs
@@ -3,19 +3,15 @@ using backend.Entities;
 namespace backend.Repositories.UserLogRepository
 {
     /// <summary>
-    /// 
+    /// Represents a repository for managing user log data.
     /// </summary>
     public interface IUserLogRepository
     {
         /// <summary>
-        /// 
+        /// Adds a user log record to the repository.
         /// </summary>
-        /// <param name="userLog"></param>
-        /// <returns></returns> <summary>
-        /// 
-        /// </summary>
-        /// <param name="userLog"></param>
-        /// <returns></returns>
+        /// <param name="userLog">The user log entity to add.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         Task AddUserLog(UserLog userLog);
     }
 }
