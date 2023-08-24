1857a2d7ffaa96d9d51b65cbe31c7ff5924796d6
Lirae Que Data <liraedata59@gmail.com>
2023-08-24 23:33:19 +0800

    Add XML Documentation in UserLogService

--- Services/UserLogService/UserLogService.cs
+++ Services/UserLogService/UserLogService.cs
@@ -5,16 +5,17 @@ using backend.Repositories.UserLogRepository;
 namespace backend.Services.UserLogService
 {
     /// <summary>
-    /// 
+    /// Service class for managing user log operations.
     /// </summary>
     public class UserLogService : IUserLogService
     {
         private readonly IUserLogRepository _userLogRepository;
 
         /// <summary>
-        /// 
+        /// Initializes a new instance of the <see cref="UserLogService"/> class.
         /// </summary>
-        /// <param name="userLogRepository"></param>
+        /// <param name="userLogRepository">The user log repository.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="userLogRepository"/> is null.</exception>
         public UserLogService(IUserLogRepository userLogRepository)
         {
             _userLogRepository = userLogRepository ?? throw new ArgumentNullException(nameof(userLogRepository));
@@ -23,7 +24,8 @@ namespace backend.Services.UserLogService
         /// <inheritdoc />
         public async Task LogUserAuthentication(User user, string description, UserEventType type)
         {
-            var userLog = new UserLog() {
+            var userLog = new UserLog()
+            {
                 UserId = user.Id,
                 User = user,
                 EventDescription = description,
